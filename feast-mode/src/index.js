83f13f09588d72f1274c9da1091bdea7505508da
Summer 19 <cssummer19@Summers-iMac.local>
2019-06-20 15:21:37 -0700

    home page

--- feast-mode/src/index.js
+++ feast-mode/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App'
 
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 serviceWorker.unregister();
