4a75d72a9d1c50cfc32e9798457d96481d9d7a90
Summer 19 <summer19@Summers-iMac.local>
2019-06-20 13:18:09 -0700

    routes

---
+++ feast-mode/src/components/SignIn/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SignIn = () => (
+  <div>
+    <h1>SignIn</h1>
+  </div>
+
+);
+
+export default SignIn
--- feast-mode/src/index.js
+++ feast-mode/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
