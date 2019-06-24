31b89007dd9a23c4190918b14ff8360fc71b17b1
Summer 19 <cssummer19@Summers-iMac.local>
2019-06-24 15:38:47 -0700

    Landing updated

--- feast-mode/src/components/Navigation/index.js
+++ feast-mode/src/components/Navigation/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
 
