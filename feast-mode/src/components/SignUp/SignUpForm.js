7df89e21174f765c626610ff525d4fa1c65c812c
Summer 19 <cssummer19@Summers-iMac.local>
2019-06-28 15:03:08 -0700

    Error messages kinda

--- feast-mode/src/components/SignUp/SignUpForm.js
+++ feast-mode/src/components/SignUp/SignUpForm.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
 import { compose } from 'recompose';
+import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 const INITIAL_STATE = {
     firstName: '',
@@ -12,7 +14,15 @@ const INITIAL_STATE = {
     passwordTwo: '',
     email: '',
     phone: '',
-    error: null,
+    touched: {
+        firstName: false,
+        lastName: false,
+        username: false,
+        passwordOne: false,
+        passwordTwo: false,
+        email: false,
+        phone: false,
+    }
 };
 
 class SignUpFormBase extends Component {
@@ -24,22 +34,8 @@ class SignUpFormBase extends Component {
     handleSubmit = event => {
         const { username, email, phone, passwordOne, passwordTwo } = this.state;
         
-        const isInvalid = (
-            passwordOne !== passwordTwo ||
-            passwordOne === '' ||
-            email === '' ||
-            phone === '' ||
-            username === ''
-        )
         event.preventDefault();
 
-        if (isInvalid === true) {
-            if (passwordOne === '' || email === '' || phone === '' || username === ''){
-                alert("Please fill out all fields")
-            } else {
-                alert("Passwords must match")
-            }
-        } else {
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
@@ -52,124 +48,138 @@ class SignUpFormBase extends Component {
                 phone,
                 })
             })
-            .then(authUser => {
+            .then(() => {
             this.setState({ ...INITIAL_STATE });
             this.props.history.push('/pay');
             })
             .catch(error => {
             this.setState({ error });
             });
-        }
     }
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
-    };
+    }
 
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: {
+                ...this.state.touched, [field]: true
+            }
+        })
+    }
+
+    validate = () => {
+        return {
+            firstName: this.state.firstName.length == 0,
+            lastName: this.state.lastName.length == 0,
+            username: this.state.username.length == 0,
+            passwordOne: this.state.passwordOne.length == 0,
+            passwordTwo: this.state.passwordTwo != this.state.passwordOne,
+            phone: this.state.phone.length == 0,
+            email: this.state.email.length == 0
+        }
+    }
+    
     render() {
-
-        const {
-            firstName,
-            lastName,
-            username,
-            passwordOne,
-            passwordTwo,
-            email,
-            phone,
-            error,
-        } = this.state;
-
-        // When the form is invalid, the submit button is disabled. 
-        // Here is when the button is disabled: 
-        const isInvalid =
-        passwordOne !== passwordTwo ||
-        passwordOne === '' ||
-        email === '' ||
-        phone === '' ||
-        username === '';
-
+        const errors = this.validate()
+        const shouldMarkError = (field) => {
+            const hasError = errors[field]
+            const shouldShow = this.state.touched[field]
+            return hasError && shouldShow
+        }
 
         return (
             <form onSubmit={this.handleSubmit} className = "classic-form">
 
                 <div className = "aligned-inputs"> 
-                    <div>
+                    <div className = {shouldMarkError("firstName") && "error"}>
                         <label> First Name </label> <br />
                         <input
                         name = "firstName"
-                        value = {firstName}
+                        value = {this.state.firstName}
                         onChange = {this.handleChange}
+                        onBlur = {this.handleBlur("firstName")}
                         type = "text"
                         /> <br />
+                        <p className = "error-text" style = {{ display: shouldMarkError("firstName") ? "block" : "none" }}> Please enter your name </p>
                     </div>
 
                     <div>
                         <label> Last Name </label> <br />
                         <input
                         name = "lastName"
-                        value = {lastName}
+                        value = {this.state.lastName}
                         onChange = {this.handleChange}
+                        onBlur = {this.handleBlur("lastName")}
                         type = "text"
                         /> <br/>
                     </div>
                 </div>
 
-                <div className = "compensate-input">
+                <div className = {["compensate-input", shouldMarkError("username") && "error"].join(" ")}>
                     <label> Username </label> <br />
                     <input
                     name = "username"
-                    value = {username}
+                    value = {this.state.username}
                     onChange = {this.handleChange}
+                    onBlur = {this.handleBlur("username")}
                     type = "text"
                     /> <br/>
+                    <p className = "error-text" style = {{ display: shouldMarkError("username") ? "block" : "none" }}> Please enter a username </p>
                 </div>
                 
-                <div className = "compensate-input"> 
+                <div className = {["compensate-input", shouldMarkError("email") && "error"].join(" ")}> 
                     <label> Email </label> <br />
                     <input
                     name = "email"
-                    value = {email}
+                    value = {this.state.email}
                     onChange = {this.handleChange}
+                    onBlur = {this.handleBlur("email")}
                     type = "text"
                     /> <br/>
+                    <p className = "error-text" style = {{ display: shouldMarkError("email") ? "block" : "none" }}> Please enter your email </p>
                 </div>
                 
-                <div className = "compensate-input">
+                <div className = {["compensate-input", shouldMarkError("phone") && "error"].join(" ")}>
                     <label> Phone Number </label> <br />
                     <input
                     name = "phone"
-                    value = {phone}
+                    value = {this.state.phone}
                     onChange = {this.handleChange}
+                    onBlur = {this.handleBlur("phone")}
                     type = "text"
                     /> <br/>
+                    <p className = "error-text" style = {{ display: shouldMarkError("phone") ? "block" : "none" }}> Please enter your phone number </p>
                 </div>
                 
                 <div className = "aligned-inputs"> 
-                    <div>
+                    <div className = {shouldMarkError("passwordOne") && "error"}>
                         <label> Password </label> <br />
                         <input
                         name = "passwordOne"
-                        value = {passwordOne}
+                        value = {this.state.passwordOne}
                         onChange ={ this.handleChange}
+                        onBlur = {this.handleBlur("passwordOne")}
                         type = "password"
                         /> <br/>
+                        <p className = "error-text" style = {{ display: shouldMarkError("passwordOne") ? "block" : "none" }}> Please enter a password </p>
                     </div>
                     
-                    <div>
+                    <div className = {shouldMarkError("passwordTwo") && "error"}>
                         <label> Confirm Password </label> <br />
                         <input
                         name = "passwordTwo"
-                        value = {passwordTwo}
+                        value = {this.state.passwordTwo}
                         onChange = {this.handleChange}
+                        onBlur = {this.handleBlur("passwordTwo")}
                         type = "password"
                         /> <br/>
+                        <p className = "error-text" style = {{ display: shouldMarkError("passwordTwo") ? "block" : "none" }}> Passwords don't match </p>
                     </div>
                 </div>
 
-                <button onMouseOver = {this.handleClick} type = "submit" className = "classic-button"> Next </button>
-
-                {/* disabled={isInvalid} */}
-                {error && <p>{error.message}</p>}
+                <button type = "submit" className = "classic-button"> Next </button>
 
             </form>
         );
@@ -188,4 +198,4 @@ const SignUpLink = () => (
 );
 
 export default SignUpForm
-export { SignUpLink, INITIAL_STATE }
+export { SignUpLink, INITIAL_STATE };
