d0a62de5c43e5638f2f3c2c137d6431fe22dcd94
Summer 19 <cssummer19@Summers-iMac.local>
2019-07-03 16:33:50 -0700

    Style

--- feast-mode/src/components/App/index.js
+++ feast-mode/src/components/App/index.js
@@ -73,7 +73,7 @@ const App = ({ loggedIn, sideDrawer, emailVerified, open, close }) => {
           <Route exact path={ROUTES.SIGN_OUT} component={SignOut} />
           <Route exact path={ROUTES.MENU} component={Menu} />
           <Route exact path={ROUTES.PASSWORD_CHANGE} component={PasswordChangePage} />
-          {/* <Route exact path={ROUTES.PROFILE_CHANGE} component={ProfileChangePage} /> */}
+          <Route exact path={ROUTES.PROFILE_CHANGE} component={ProfileChangePage} />
           <Route exact path={ROUTES.RESTAURANTS} component={Restaurants} />
           <Route exact path={ROUTES.ABOUT} component={About} />
           <Route exact path={ROUTES.FRIENDS} component={Friends} />
