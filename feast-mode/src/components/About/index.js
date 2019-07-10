dea85121c20c9090ffac42df5347996f0767b005
Summer 19 <summer19@Summers-iMac.local>
2019-07-10 09:19:50 -0700

    styled components

--- feast-mode/src/components/About/index.js
+++ feast-mode/src/components/About/index.js
@@ -11,42 +11,61 @@ const Roles = styled.div`
 const Bios = styled(Roles)`
   margin: 10px; 
 `
-// END OF STYLED COMPONENTS 
+const Text = styled.h1`
+  padding-top: 20px;
+  font-size: 50px;
+`
+const Grid = styled.section`
+  padding-bottom: 25px;
+  display: grid;
+  grid-template-columns: auto auto auto;
+`
+const Window = styled.div`
 
-const AboutPage = () => (
-  <div>
-    <section class="center">
-      <div class="aboutus">
-        <h1>About Us</h1>
-      </div>
-    </section>
+  justify-content: space-around;
+  text-align: center;
+`
 
-
-    <Roles>
-        <p id= "alexandra">Developer</p>
-        <p id= "ignacio">Developer</p>
-        <p id= "cher">Developer</p>
-    </Roles>
-
-    <section class="grid">
-    <Bios>
-      <p id= "a1">
-        <i class="textinc">Alexandra</i> is a Harvey Mudd student .....
-      </p>
-    </Bios>
-    <Bios>
-      <p id= "i1">
-        <i class="textinc">Ignacio</i> is a Harvey Mudd student .....
-      </p>
-    </Bios>
-    <Bios>
+const Cher = () => (
+  <Bios>
       <p id= "c1">
         <i class=" textinc">Cher</i> is a Harvey Mudd student .....
       </p>
-   </Bios>
-   </section>
+  </Bios>
+)
 
-  </div>
+const Alexandra = () => (
+  <Bios>
+      <p id= "c1">
+        <i class=" textinc">Alexandra</i> is a Harvey Mudd student .....
+      </p>
+  </Bios>
+)
+
+const Ignacio = () => (
+  <Bios>
+      <p id= "c1">
+        <i class=" textinc">Ignacio</i> is a Harvey Mudd student .....
+      </p>
+  </Bios>
+)
+
+// END OF STYLED COMPONENTS 
+
+const AboutPage = () => (
+  <Window>
+    <Text>About Us</Text>
+    <Roles>
+      <p id="alexandra">Developer</p>
+      <p id="ignacio">Developer</p>
+      <p id="cher">Developer</p>
+    </Roles>
+    <Grid>
+      <Alexandra />
+      <Ignacio />
+      <Cher />
+    </Grid>
+  </Window>
 );
 
 export default AboutPage;
