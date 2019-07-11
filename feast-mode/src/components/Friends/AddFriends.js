136218c33eba888cdc63e8bf38252e72088cf73b
Summer 19 <summer19@Summers-iMac.local>
2019-07-11 15:11:42 -0700

    popup

--- feast-mode/src/components/Friends/AddFriends.js
+++ feast-mode/src/components/Friends/AddFriends.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import {firestoreConnect} from 'react-redux-firebase';
 import { compose } from 'redux';
 
+
+
 import Friend from './Friend';
 import AddFriendButton from './Buttons/AddFriendButton';
 import Loader from '../Loader/index.js';
@@ -10,12 +12,13 @@ import Modal from '../Modal/Modal'
 import Button from '../../style/FormUI/Buttons'
 import './AddFriends.css';
 
-const AddFriends = ({users, userId, allFriends, close, opened, hasRequested }) => {
+import * as actions from '../../backend/store/actions/';
+
+const AddFriends = ({addFriend, users, userId, allFriends, close, opened, hasRequested }) => {
 
   const [isOpened, setisOpened] = useState(false);
 
-
-  console.log("wh")
+  // console.log("wh")
   let content;
   
   if(!users)
@@ -29,48 +32,47 @@ const AddFriends = ({users, userId, allFriends, close, opened, hasRequested }) =
   
   else if( users.length === 0 )
   {
-    console.log("elif")
+    // console.log("elif")
     content = (
       <p>There are no users!</p>
     )
   }
   else
   {
-    console.log("else")
+    // console.log("else")
     
     users = Object.keys(users).filter(user => user !== userId)
   
-    let friends = [];
+    // let friends = [];
     // console.log(friends)
     
-    if(allFriends.friends)
-    {
-      friends += allFriends.friends
-    }
+    // if(allFriends.friends)
+    // {
+    //   friends += allFriends.friends
+    // }
       
-    if(allFriends.requests) 
-    {
-      friends += allFriends.requests
-    }
-      
+    // if(allFriends.requests) 
+    // {
+    //   friends += allFriends.requests
+    // }
 
-    console.log(!friends.includes(userId))
 
-    users = users.filter(user => !friends.includes(user) && user !== userId )
+    // console.log(!friends.includes(userId))
+
+    // users = users.filter(user => !friends.includes(user) && user !== userId )
     content = (
       <div>
         {
           users.map(user => 
           <div className="friend" key={user}>
-            <Friend display={true} friend={user} />
-            <AddFriendButton friend={user}/>
+              <Friend display={true} friend={user} />
+              <AddFriendButton friend={user}/>
           </div>
         )}
         
       </div>
-      
     )
-    console.log("over here")
+    // console.log("over here")
   }
  
 
@@ -82,12 +84,15 @@ const AddFriends = ({users, userId, allFriends, close, opened, hasRequested }) =
     <Modal opened={isOpened} close={() => setisOpened(false)}>
       <Button color="main" contain onClick={() => setisOpened(false)}>
         Done
-      </Button>
-      <div className = "friends-change">
-        <h1> All Users </h1>
-        {content}
+      </Button >
+      <div className = "friends-change"> 
+      <h1> All Users </h1>
+      {content}
       </div>
     </Modal>
+      
+
+   
     </>
   )
 }
@@ -107,3 +112,11 @@ export default compose(
   firestoreConnect(props => ["users/"]),
   firestoreConnect(props => ["friends/"]),
 )(AddFriends)
+
+
+
+
+/*
+ 
+
+  */
