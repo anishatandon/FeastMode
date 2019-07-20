6aabcf6c3ce894092941a1acd131817cdbb29333
Summer 19 <summer19@Summers-iMac.local>
2019-07-19 17:02:24 -0700

    map shows up

--- feast-mode/client/src/components/Location/MapContainer.js
+++ feast-mode/client/src/components/Location/MapContainer.js
@@ -4,23 +4,48 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const GMAPS_API_KEY = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo" //borrowed from tutorial
 
+//note: code formatted for ES6 here
 export class MapContainer extends Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
-
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow> */}
-      </Map>
-    );
+    state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  
+    onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+  
+    onMapClicked = (props) => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
+    };
+  
+    render() {
+      return (
+        <Map google={this.props.google}
+            onClick={this.onMapClicked}>
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+  
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      )
+    }
   }
-}
 
 export default GoogleApiWrapper({
   apiKey: GMAPS_API_KEY
--- feast-mode/client/src/components/Location/index.js
+++ feast-mode/client/src/components/Location/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import MapContainer from './MapContainer.js'
-
-
-
+import Button from '../../style/FormUI/Buttons'
 
 const DisplayMap = () => {
     return(
+        <>
+        <h2>Where do you want to delver to?</h2>
+        <div>Search bar here</div>
+        <Button> Current Location </Button>
         <MapContainer />
+        </>
     )
 }
 
